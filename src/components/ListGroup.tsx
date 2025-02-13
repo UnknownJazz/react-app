611faa33ada8dd00068efbeaf41a85f52e3f1db4
UnknownJazz <114299193+UnknownJazz@users.noreply.github.com>
2025-02-13 11:50:25 +0800

    Added conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,20 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {  
-    const items = [
+    let items = [
         'Chas',
         'Shaira',
         'Cyreh',
         'Marian'
     ]
+    items = []
 
     return  (
         <Fragment>
             <h1>List</h1>
+            {items.length === 0 && <p>No items found</p>}
             <ul className="list-group">
-                {items.map(item => <li key={item}>{item}</li>)}
+                {items.map((item) => <li key={item}>{item}</li>)}
             </ul>
         </Fragment>
     );
